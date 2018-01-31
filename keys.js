4cda134a6f92151a6671a91f3e0859b23d903b20
randall ishino <rishino24@gmail.com>
2018-01-30 21:42:16 -0800

    configuring twitter keys

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -12,4 +12,4 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-console.log(exports.twitter);
+// console.log(exports.twitter);
--- liri.js
+++ liri.js
@@ -1,5 +1,45 @@
+require('dotenv').config()
+
 var fs = require('fs');
 var keys = require('./keys.js');
 var spotify = require('spotify');
-var twitter = require('twitter');
-var client = new Twitter(keys.twitter);
+var Twitter = require('twitter');
+var request = require('request');
+
+
+var options = [
+    'my-tweets',
+    'spotify-this-song',
+    'movie-this',
+    'do-what-it-says'
+];
+
+var command = process.argv[2];
+
+function commandCheck() {
+    if (command === 'my-tweets') {
+        tweets();
+    }
+}
+
+
+function tweets() {
+var params = {screen_name: 'NBA'};
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+    else {
+        console.log(error);
+    }
+})
+};
+
+commandCheck();
