08eb2e1f31aef2e943bab5d740a7ca4be36ae335
randall ishino <rishino24@gmail.com>
2018-01-30 22:03:09 -0800

    fixed twitter keys bug

--- liri.js
+++ liri.js
@@ -6,6 +6,14 @@ var spotify = require('spotify');
 var Twitter = require('twitter');
 var request = require('request');
 
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+
 
 var options = [
     'my-tweets',
@@ -25,12 +33,6 @@ function commandCheck() {
 
 function tweets() {
 var params = {screen_name: 'NBA'};
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
 
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
