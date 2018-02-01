adfb19ca932c8cccd61c626add9f5f3a499c1316
randall ishino <rishino24@gmail.com>
2018-01-31 16:11:53 -0800

    spotify working

--- liri.js
+++ liri.js
@@ -86,12 +86,18 @@ function getSpotify() {
             return;
         }
         else {
-            // console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
-            // console.log("Song name: " + data.tracks.items[0].name);
+            console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+            console.log("Song name: " + data.tracks.items[0].name);
             console.log("Preview link: " + data.tracks.items[0].album.external_urls.spotify);
+            console.log("Album name: " + data.tracks.items[0].album.name);
         }
 });
-};  
+}; 
+
+
+function OMDB() {
+    
+}
 
 
 
